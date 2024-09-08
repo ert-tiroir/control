12a572f51bc50d0395f26ae154e40ecb6fb9c572
theo.hollender <theo.hollender@gmail.com>
2024-09-08 14:05:25 +0200

    control.core: added app and start_core

---
+++ control/core/__init__.py
@@ -0,0 +1,21 @@
+
+import sys
+
+from control.core.app import init_applications, stop_applications
+
+def start_control ( settings: str ):
+    from control.config.manager import ConfigManager
+
+    ConfigManager().import_config( settings )
+
+    assert "control.config.include" not in sys.modules, "control.config.include was imported too early"
+    
+    # Lock config
+    import control.config.include
+
+    init_applications()
+def stop_control ():
+    from control.config.manager import ConfigManager
+    if not ConfigManager().has_config(): return
+    
+    stop_applications()
---
+++ control/core/app.py
@@ -0,0 +1,61 @@
+
+import importlib
+import importlib.util
+from types import ModuleType
+from typing import List
+from control.utils.singleton import Singleton
+
+class ApplicationManager(metaclass=Singleton):
+    applications: "List[Application]"
+    def __init__(self) -> None:
+        self.applications = []
+    
+    def bind (self, path: str):
+        self.path = path
+        self._lpt = path
+    def unbind (self):
+        assert self.path is None, f"Improperly configured, could not find app for {self.path}"
+    def add_application (self, application_type):
+        assert self.path is not None, f"Improperly configured, two apps are being imported for {self._lpt}"
+
+        try:
+            self.applications.append( application_type(self.path) )
+        except Exception:
+            assert False, f"Could not instantiate app {self.path}"
+        self.path = None
+
+class Application(metaclass=Singleton):
+    def __init__(self, path: str) -> None:
+        self.__path = path
+    def __init_subclass__(cls) -> None:
+        ApplicationManager().add_application(cls)
+    def init_application (self):
+        pass
+    def stop_application (self):
+        pass
+    def load_module (self, module: str) -> None | ModuleType:
+        target = self.path + "." + module
+
+        spec = importlib.util.find_spec(target)
+        if spec is None: return None
+
+        return importlib.import_module(target)
+
+    @property
+    def path (self):
+        return self.__path
+
+def init_applications ():
+    from control.config.include import ENABLED_APPS
+
+    manager = ApplicationManager()
+    for application in ENABLED_APPS:
+        manager.bind(application)
+        importlib.import_module( application + ".app" )
+
+        manager.unbind()
+    for application in manager.applications:
+        application.init_application()
+def stop_applications ():
+    for application in ApplicationManager().applications:
+        application.stop_application()
