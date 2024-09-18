14f9f54c6a6916520271e9848bef3ed4123e6273
theo.hollender <theo.hollender@gmail.com>
2024-09-18 11:03:58 +0200

    1.x: added test on ground config

---
+++ config/avionics/test_on_ground.py
@@ -0,0 +1,44 @@
+
+########################################################
+# TEST ON GROUND AVIONICS CONFIGURATION                #
+# ===========================+======================== #
+# Applications               | Sensors                 #
+#                            | Camera                  #
+#                            | Network Controller      #
+# ===========================+======================== #
+# Sensor List                | DPS310                  #
+#                            | BNO055                  #
+# Sensor Autostart           | False                   #
+# Sensor Mode                | WRITER                  #
+# ===========================+======================== #
+# Camera Mode                | WRITER                  #
+# Camera Autostart           | False                   #
+# Camera Command             | libcamera-vid -t 0 -o - #
+# ===========================+======================== #
+# Control Chain              | None                    #
+# Model Chain                | Network Controller      #
+########################################################
+
+from control.config.default.settings import *
+from control.contrib.netc.sender import send_to_netc
+from control.contrib.sensors.devices.dps310 import DPS310Device
+
+ENABLED_APPS = [
+    "control.contrib.camera",
+    "control.contrib.sensors",
+    "control.contrib.netc"
+]
+
+SENSORS_LIST = [
+    DPS310Device()
+]
+
+SENSORS_AUTOSTART = False
+SENSORS_MODE      = "WRITER"
+
+CAMERA_AUTOSTART = False
+CAMERA_COMMAND   = [ "libcamera-vid", "-t", "0", "-o", "-"]
+CAMERA_MODE      = "WRITER"
+
+NEXT_ON_CONTROLLER_CHAIN = None
+NEXT_ON_MODEL_CHAIN      = send_to_netc()
--- manage.py
+++ manage.py
@@ -3,4 +3,4 @@ from control.core import start_control, stop_control
 
 if __name__ == "__main__":
     with open("target.txt", "r") as file:
-        start_control(file.read())
+        start_control(file.read().split("\n")[0])
