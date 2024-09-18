c92bbc375a8cc361dbc573c9ff9b20c885648d72
theo.hollender <theo.hollender@gmail.com>
2024-09-18 11:09:08 +0200

    1.x: fixed circular import

--- control/contrib/camera/protocol/__init__.py
+++ control/contrib/camera/protocol/__init__.py
@@ -1,5 +1,4 @@
 
-from control.config import settings
 from control.contrib.camera.protocol.control.start import StartCameraPacket, start_camera
 from control.contrib.camera.protocol.control.stop import StopCameraPacket, stop_camera
 from control.contrib.camera.protocol.model.data import DataPacket, handle_data_packet
@@ -19,7 +18,7 @@ physical = [
     ("/camera/control/start", StartCameraPacket, start_camera),
     ("/camera/control/stop",  StopCameraPacket,  stop_camera),
 
-    ("/camera/model/media",       DataPacket,    handle_data_packet),
+    ("/camera/model/media",       DataPacket,    handle_data_packet()),
     ("/camera/model/event/start", OnStartCamera, model_send_and_flush),
     ("/camera/model/event/end",   OnEndCamera,   model_send_and_flush)
 ]
--- control/contrib/camera/protocol/model/data.py
+++ control/contrib/camera/protocol/model/data.py
@@ -1,5 +1,5 @@
 
-from control.contrib.camera.app import CameraApplication
+
 from control.contrib.protocol.fields.bytes import BytesField
 from control.contrib.protocol.fields.integer import IntegerField
 from control.contrib.protocol.fields.packet import MultiField
@@ -8,5 +8,12 @@ class DataPacket(MultiField):
     id   = IntegerField()
     data = BytesField()
 
-def handle_data_packet (packet: DataPacket):
-    CameraApplication().on_data(packet)
+class handle_data_packet ():
+    def __init__(self) -> None:
+        self.app = None
+    def __call__(self, *args, **kwargs):
+        if self.app is None:
+            from control.contrib.camera.app import CameraApplication
+
+            self.app = CameraApplication()
+        self.app.on_data(*args, **kwargs)
